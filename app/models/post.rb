5f87c7d651984ecff4e3a32e46fb3213ef1555d5
Damir-Che <chechuhov.damir@mail.ru>
2020-10-11 15:09:42 +0300

    Comment(model/controller) for Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   validates :title, :body, presence: true #проверка на не пустое значение
   belongs_to :user
-  has_one_attached :image
+  has_one_attached :image #ActiveStorage
+  has_many :comments
 end
